9fc0dc7a97dfe6eb1d6dc3d65680f0f536b61f30
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-06-18 14:55:26 +0000

    gh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function calculateProduct() {
+    let sum = 0;
+    let numbers = "";
+  
+    // Get the values from the inputs
+    let userNum1 = parseInt(document.getElementById('userNum1').value); // how many times
+    let userNum2 = parseInt(document.getElementById('userNum2').value); // number to add
+  
+    // Loop to add the second number as many times as the first number
+    for (let counter = 1; counter <= userNum1; counter++) {
+      sum += userNum2;
+  
+      // Build the string: "5 + 5 + 5" etc.
+      if (counter < userNum1) {
+        numbers += userNum2 + " + ";
+      } else {
+        numbers += userNum2;
+      }
+    }
+  
+    // Display the result in HTML
+    document.getElementById('display-results').innerHTML = numbers + " = " + sum;
+  }
+  
